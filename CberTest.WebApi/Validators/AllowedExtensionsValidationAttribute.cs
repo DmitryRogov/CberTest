3ba3029b7519a7132a828db58ebdfc8409a1795b
Rogov Dmitry Igorevich <idmitryrogov@gmail.com>
2020-08-27 21:11:35 +0300

    Исправить аттрибут валидации расширений файла

--- CberTest.WebApi/Validators/AllowedExtensionsValidationAttribute.cs
+++ CberTest.WebApi/Validators/AllowedExtensionsValidationAttribute.cs
@@ -18,10 +18,10 @@ namespace CberTest.WebApi.Validators
         protected override ValidationResult IsValid(
         object value, ValidationContext validationContext)
         {
-            var file = value as IFormFile;
-            var extension = Path.GetExtension(file.FileName);
+            var file = value as IFormFile;         
             if (file != null)
             {
+                var extension = Path.GetExtension(file.FileName);
                 if (!_extensions.Contains(extension.ToLower()))
                 {
                     return new ValidationResult(GetErrorMessage());
